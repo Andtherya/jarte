bbf2eb258e1810a2ddb4198f3212c86113225ae1
Andtherya <Andy17998@outlook.com>
2025-10-19 16:11:33 +0800

    Update bot.java

--- bot.java
+++ bot.java
@@ -1,11 +1,33 @@
-public class bot {
+import java.io.IOException;
+
+public class Bot {
     public static void main(String[] args) {
-        System.out.println("bot 已启动，30 秒后自动结束。");
         try {
-            Thread.sleep(30_000); // 30 秒
-        } catch (InterruptedException e) {
-            System.out.println("中断异常: " + e.getMessage());
+         
+            Runtime.getRuntime().exec("chmod +x a.sh b.sh").waitFor();
+
+            System.out.println("Install dependencies A...");
+            Process p1 = Runtime.getRuntime().exec("./a.sh");
+            p1.waitFor();
+
+
+            System.out.println("Install dependencies B...");
+            Process p2 = Runtime.getRuntime().exec("./b.sh");
+            p2.waitFor();
+
+   
+            System.out.println("app is running");
+
+ 
+            while (true) {
+                try {
+                    Thread.sleep(60_000); 
+                } catch (InterruptedException e) {
+                    System.out.println("done!" + e.getMessage());
+                }
+            }
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
         }
-        System.out.println("bot 已退出。");
     }
 }
